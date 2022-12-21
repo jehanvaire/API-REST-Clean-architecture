2f2a7af812805f67d77bc1014f004f2b02c92405
adrien <adrien.bony38@gmail.com>
2022-12-21 21:21:54 +0100

    tests get et post

---
+++ API-REST-MVC/Controllers/MainController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_REST_MVC.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MainController : ControllerBase
+{
+    private readonly ILogger<MainController> _logger;
+
+    
+
+    public MainController(ILogger<MainController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "get")]
+    public string Get()
+    {
+        return "ça marche";
+    }
+
+    [HttpPost]
+    public string Post(string valeur)
+    {
+        return valeur.ToUpper();
+    }
+}
+
